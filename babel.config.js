fabeb186eadbb519bed565b99ddc2f0fe253fda5
100ook <100ook@5works.co.kr>
2023-03-21 17:27:34 +0900

    Movies Screen part one: set Swiper and fetch NowPlaying Movies

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,33 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["module:metro-react-native-babel-preset", "babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [
+            ".ios.ts",
+            ".android.ts",
+            ".ts",
+            ".ios.tsx",
+            ".android.tsx",
+            ".tsx",
+            ".jsx",
+            ".js",
+            ".json",
+          ],
+          alias: {
+            "@": "./src",
+            "@components": "./src/components",
+            "@assets": "./src/assets",
+            "@modules": "./src/modules",
+            "@screens": "./src/screens",
+            "@utils": "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
---
+++ src/screens/Movies.tsx
@@ -0,0 +1,43 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import React from "react";
+import { Dimensions } from "react-native";
+import styled from "styled-components/native";
+import Swiper from "react-native-web-swiper";
+
+const API_KEY = "46f8bbb5cb6e87b367033c9136c8977";
+
+const Container = styled.ScrollView`
+  background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const View = styled.View`
+  flex: 1;
+`;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const getNowPlaying = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+    );
+  };
+
+  return (
+    <Container>
+      <Swiper
+        loop
+        timeout={3.5}
+        controlsEnabled={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+      </Swiper>
+    </Container>
+  );
+};
+
+export default Movies;
